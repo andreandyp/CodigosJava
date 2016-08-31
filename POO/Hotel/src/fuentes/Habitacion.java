f6a6cf82cdd5c2b8086d417377966541a971f167
alumno <alumno@PC-24>
2016-08-31 10:19:50 -0500

    Hotel

---
+++ POO/Hotel/src/fuentes/Habitacion.java
@@ -0,0 +1,12 @@
+package fuentes;
+public class Habitacion {
+    private int num;
+    public Habitacion() {
+        
+    }
+    
+    //La clase principal es la que tiene el main
+    public double sumaCuadrado(int x){
+        return Math.pow((x+3),2);
+    }
+}
