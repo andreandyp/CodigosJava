f74571d608e826604ee4aa64e04a558fdeeb2c3b
André Michel Andy Pozos <andreandyp@outlook.com>
2016-10-16 12:50:46 -0500

    Practica 3 - Redes

--- POO/Practica3 - Redes/src/practica3/redes/Practica3Redes.java
+++ POO/Practica3 - Redes/src/practica3/redes/Practica3Redes.java
@@ -2,7 +2,6 @@ package practica3.redes;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import org.jnetpcap.Pcap;
 import org.jnetpcap.PcapIf;
 import org.jnetpcap.packet.PcapPacket;
@@ -27,10 +26,8 @@ public class Practica3Redes {
       }
       buf.append(Integer.toHexString((b < 0) ? b + 256 : b).toUpperCase());
     }
-
     return buf.toString();
   }
-
 	public static void main(String[] args) {
             Pcap pcap=null;
             List<PcapIf> alldevs = new ArrayList<PcapIf>();
@@ -42,50 +39,123 @@ public class Practica3Redes {
                 return;
             }
             PcapPacketHandler<String> jpacketHandler = new PcapPacketHandler<String>() {
-                
                 public void nextPacket(PcapPacket packet, String user) {
-                    
-                    System.out.printf("\n\nPaquete recibido el %s caplen=%-4d longitud=%-4d %s\n\n",
+                    System.out.printf("\nPaquete recibido el %s caplen=%-4d longitud=%-4d %s\n",
                             new Date(packet.getCaptureHeader().timestampInMillis()),
                             packet.getCaptureHeader().caplen(),  // Length actually captured
                             packet.getCaptureHeader().wirelen(), // Original length
                             user                                 // User supplied object
                     );
-                    
-                    
                     /******Desencapsulado********/
                     for(int i=0;i<packet.size();i++){
                         System.out.printf("%02X ",packet.getUByte(i));
                         
                         if(i%16==15)
-                            System.out.println("");
+                            System.out.println();
                     }//if
-                    
                     int longitud = (packet.getUByte(12)*256)+packet.getUByte(13);
-                    System.out.printf("\nLongitud: %d (%04X)",longitud,longitud );
+                    System.out.printf("\nLongitud: (%04X) ",longitud );
                     if(longitud<1500){
-                        System.out.println("--->Trama IEEE802.3");
-                        System.out.printf(" |-->MAC Destino: %02X:%02X:%02X:%02X:%02X:%02X",packet.getUByte(0),packet.getUByte(1),packet.getUByte(2),packet.getUByte(3),packet.getUByte(4),packet.getUByte(5));
-                        System.out.printf("\n |-->MAC Origen: %02X:%02X:%02X:%02X:%02X:%02X",packet.getUByte(6),packet.getUByte(7),packet.getUByte(8),packet.getUByte(9),packet.getUByte(10),packet.getUByte(11));
-                        System.out.printf("\n |-->Longitud: %02X",longitud);
-                        System.out.printf("\n |-->DSAP: %02X",packet.getUByte(14));
-                        int ssap = packet.getUByte(15)& 0x00000001;
-                        String c_r = (ssap==1)?"Respuesta":(ssap==0)?"Comando":"Otro";
-                        System.out.printf("\n |-->SSAP: %02X   %s",packet.getUByte(15), c_r);
-                        System.out.println("Tipo de trama: ");
+                        System.out.println("(Trama IEEE802.3)");
+                        System.out.printf("\nMAC Destino: %02X:%02X:%02X:%02X:%02X:%02X",packet.getUByte(0),packet.getUByte(1),packet.getUByte(2),packet.getUByte(3),packet.getUByte(4),packet.getUByte(5));
+                        System.out.printf("\nMAC Origen: %02X:%02X:%02X:%02X:%02X:%02X",packet.getUByte(6),packet.getUByte(7),packet.getUByte(8),packet.getUByte(9),packet.getUByte(10),packet.getUByte(11));
+                        System.out.printf("\nLongitud: %d",longitud);
+                        System.out.printf("\nDSAP:");
+                        System.out.printf("\n\t%02X (Individual)",packet.getUByte(14));
                         
-                        System.out.println("N(s):");
-                        System.out.println("N(r):");
-                        System.out.println("Código:");
+                        
+                        int ssap = packet.getUByte(15)& 0x00000001;
+                        String c_r = (ssap==1)?"(Respuesta)":(ssap==0)?"(Comando)":"(Otro)";
+                        System.out.printf("\nSSAP:");
+                        System.out.printf("\n\t%02X %s",packet.getUByte(15), c_r);
+                        
+                        System.out.println("\nCampo de control:");
+                        tipoTrama(packet.getUByte(16),packet.getUByte(17));
                         
                     } else if(longitud>=1500){
                         System.out.println("-->Trama ETHERNET");
                     }//else
-                    
-                    
                     //System.out.println("\n\nEncabezado: "+ packet.toHexdump());
-                    
-                    
+                }
+
+                private void tipoTrama(int byte16,int byte17) {
+                    String ns = "",nr = "",codigo = "";
+                    int temp = 0;
+                    StringBuilder b16,b17;
+                    b16 = new StringBuilder(Integer.toBinaryString(byte16));
+                    b17 = new StringBuilder(Integer.toBinaryString(byte17));
+                    b16.reverse();
+                    b17.reverse();
+                    while(b16.length() != 8)
+                        b16.append("0");
+                    while(b17.length() != 8)
+                        b17.append("0");
+                    if(Integer.parseInt(b16.substring(0, 1)) == 0){
+                        System.out.println("Tipo I");
+                        System.out.print("N(s): ");
+                        ns = b16.substring(1,8);
+                        for (int i = 0; i < 7; i++) {
+                            if(ns.charAt(i) == '1')
+                                temp = temp+(int)(1*Math.pow(2, i));
+                        }
+                        System.out.println(temp);
+                        System.out.print("N(r): ");
+                        nr = b17.substring(1, 8);
+                        temp = 0;
+                        for (int i = 0; i < 7; i++) {
+                            if(nr.charAt(i) == '1')
+                                temp = temp+(int)(1*Math.pow(2, i));
+                        }
+                        System.out.println(temp);
+                    }
+                    else{
+                        if(Integer.parseInt(b16.substring(1, 2)) == 0){
+                            System.out.println("\tTipo S");
+                            System.out.print("\tCódigo: ");
+                            codigo = b16.substring(2,4);
+                            switch(codigo){
+                                case "00":
+                                    System.out.println("RR");
+                                    break;
+                                case "01":
+                                    System.out.println("REJ");
+                                    break;
+                                case "10":
+                                    System.out.println("RNR");
+                                    break;
+                                case "11":
+                                    System.out.println("SREJ");
+                                    break;
+                            }
+                            System.out.print("\tN(r): ");
+                            nr = b17.substring(1, 8);
+                            temp = 0;
+                            for (int i = 0; i < 7; i++) {
+                                if (nr.charAt(i) == '1')
+                                    temp = temp + (int) (1 * Math.pow(2, i));
+                            }
+                            System.out.println(temp);
+                        }
+                        else{
+                            System.out.println("\tTipo U");
+                            System.out.print("\tCódigo: ");
+                            codigo = b16.substring(2,4);
+                            switch(codigo){
+                                case "00":
+                                    System.out.println("RR");
+                                    break;
+                                case "01":
+                                    System.out.println("REJ");
+                                    break;
+                                case "10":
+                                    System.out.println("RNR");
+                                    break;
+                                case "11":
+                                    System.out.println("SREJ");
+                                    break;
+                            }
+                        }
+                    }
                 }
             };
             pcap.loop(-1, jpacketHandler, " ");
