21fe10984932b0abbf1c8dbac62a7a841c6e47bd
André Michel Andy Pozos <andreandyp@outlook.com>
2018-05-11 11:33:49 -0500

    Emojis implementados en chat multicast

--- Redes2/ChatMulticast/src/chatmulticast/HiloEntrada.java
+++ Redes2/ChatMulticast/src/chatmulticast/HiloEntrada.java
@@ -9,6 +9,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.LinkedList;
 import javax.swing.DefaultListModel;
+import com.vdurmont.emoji.EmojiParser;
+
 
 public class HiloEntrada extends Thread {
     private MulticastSocket socket;
@@ -16,7 +18,7 @@ public class HiloEntrada extends Thread {
     private LinkedList<String> mensajes;
     public static final String MCAST_ADDR  = "230.0.0.1";
     public static final int MCAST_PORT = 9013;
-    public static final int DGRAM_BUF_LEN = 1024;
+    public static final int DGRAM_BUF_LEN = 512;
     private InetAddress group;
 
     public HiloEntrada(DefaultListModel lista) throws UnknownHostException, IOException {
@@ -43,16 +45,20 @@ public class HiloEntrada extends Thread {
 
             try {
                 socket.receive(recv);
+                System.out.println("len: "+recv.getLength());
             } catch (IOException ex) {
                 System.out.println("Valió barriga: "+ex.getMessage());
             }
             
             SimpleDateFormat fechaHora = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-            entrante = new StringBuilder("<html><head></head><body>");
+            entrante = new StringBuilder("<html><head></head><body><p>");
             entrante.append(recv.getAddress()).append(":").append(recv.getPort());
             entrante.append(" a las ").append(fechaHora.format(new Date())).append("<br>");
-            byte [] data = recv.getData();
-            entrante.append(new String(data)).append("</body></html>");
+            byte data[] = new byte[recv.getLength()];
+            for(int i = 0; i < recv.getLength(); i++){
+                data[i] = recv.getData()[i];
+            }
+            entrante.append(new String(data)).append("</p></body></html>");
             
             if(lista != null){
                 lista.addElement(entrante.toString());
--- Redes2/ChatMulticast/src/chatmulticast/MultiClient.java
+++ Redes2/ChatMulticast/src/chatmulticast/MultiClient.java
@@ -1,4 +1,5 @@
 package chatmulticast;
+import com.vdurmont.emoji.Emoji;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -12,18 +13,25 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.ListSelectionModel;
+import com.vdurmont.emoji.EmojiParser;
+import com.vdurmont.emoji.EmojiManager;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.util.Collection;
+import javax.swing.JEditorPane;
  
 public class MultiClient extends JFrame implements ActionListener{
     private String nombre = "";
     private HiloEntrada entrada;
-    private JTextArea texto;
+    private JEditorPane texto;
     private JLabel tag1, tag2, tag3;
     private JButton enviar, limpiar;
-    private JList mensajes;
+    private JList mensajes, emojis;
     private DefaultListModel listaMensajes = new DefaultListModel();
-    private JScrollPane scroll;
+    private DefaultListModel listaemojis = new DefaultListModel();
+    private JScrollPane scroll, scrollemojis;
     private MulticastSocket socket;
     public static final String MCAST_ADDR  = "230.0.0.1";
     public static final int MCAST_PORT = 9013;
@@ -32,21 +40,25 @@ public class MultiClient extends JFrame implements ActionListener{
     public MultiClient(){
         this.inicializarChat();
         super.setLayout(null);
-        super.setBounds(100, 100, 640, 480);
+        super.setSize(new Dimension(640, 480));
+        super.setLocationRelativeTo(null);
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         super.setTitle("Chat multicast");
-        texto = new JTextArea();
+        texto = new JEditorPane();
         tag1 = new JLabel("Escribe aquí");
         tag2 = new JLabel("Bienvenido "+nombre);
         tag3 = new JLabel("Mensajes recibidos");
         enviar = new JButton("Enviar");
         limpiar = new JButton("Limpiar");
         mensajes = new JList();
+        emojis = new JList();
         scroll = new JScrollPane(mensajes);
+        scrollemojis = new JScrollPane(emojis);
         tag1.setBounds(10, 35, 200, 15);
         tag2.setBounds(10, 10, 200, 15);
         tag3.setBounds(320, 35, 200, 15);
         texto.setBounds(10, 60, 300, 100);
+        texto.setFont(new Font("Segoe UI Emoji", 0, 14));
         enviar.setBounds(10, 170, 100, 25);
         limpiar.setBounds(320, 330, 100, 25);
         enviar.addActionListener(this);
@@ -54,7 +66,19 @@ public class MultiClient extends JFrame implements ActionListener{
         mensajes.setModel(listaMensajes);
         mensajes.setLayoutOrientation(JList.VERTICAL);
         mensajes.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        mensajes.setFont(new Font("Segoe UI Emoji", 0, 14));
+        Font fuentes[] = GraphicsEnvironment.getLocalGraphicsEnvironment().getAllFonts();
+        emojis.setModel(listaemojis);
+        emojis.setLayoutOrientation(JList.VERTICAL);
+        emojis.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        emojis.setFont(new Font("Segoe UI Emoji", 0, 14));
         scroll.setBounds(320, 60, 300, 260);
+        scrollemojis.setBounds(10, 200, 100, 250);
+        Collection<Emoji> emojis = EmojiManager.getAll();
+        System.out.println(emojis.size());
+        for(Emoji e : emojis){
+            listaemojis.addElement(e.getUnicode());
+        }
         super.add(tag1);
         super.add(tag2);
         super.add(tag3);
@@ -62,7 +86,7 @@ public class MultiClient extends JFrame implements ActionListener{
         super.add(enviar);
         super.add(limpiar);
         super.add(scroll);
-        
+        super.add(scrollemojis);
         super.setVisible(true);
     }
     
@@ -79,7 +103,7 @@ public class MultiClient extends JFrame implements ActionListener{
         while(nombre.equals("")){
             nombre = JOptionPane.showInputDialog(this, "", "Selecciona un nombre", JOptionPane.QUESTION_MESSAGE);
             if(nombre == null){
-            System.exit(0);    
+                System.exit(0);
             }
         }
         entrada.start();
@@ -93,8 +117,10 @@ public class MultiClient extends JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource().equals(enviar)){
-            String mensaje = nombre+" dice: "+texto.getText();
+            String mensaje = nombre+" dice: "+EmojiParser.parseToHtmlDecimal(texto.getText());
             DatagramPacket paquete = new DatagramPacket(mensaje.getBytes(), mensaje.length(), grupo, MCAST_PORT);
+            System.out.println("Paq "+paquete.getLength());
+            System.out.println("Mens "+mensaje.length());
             try {
                 socket.setTimeToLive(32);
                 socket.send(paquete);
