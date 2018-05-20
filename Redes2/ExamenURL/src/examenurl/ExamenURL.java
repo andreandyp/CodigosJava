bbb95428e5e349fb8e9d12c51379c9a7e4096b6f
André Michel Andy Pozos <andreandyp@outlook.com>
2018-05-20 14:33:01 -0500

    Ya descarga, falta incluir estilos

--- Redes2/ExamenURL/src/examenurl/ExamenURL.java
+++ Redes2/ExamenURL/src/examenurl/ExamenURL.java
@@ -1,14 +1,15 @@
 package examenurl;
 
-import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.locks.ReentrantLock;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -21,20 +22,24 @@ public class ExamenURL{
     
     public ExamenURL() throws InterruptedException{
         String url = "https://andreandyp.github.io";
+        int profundidad = 0;
         rl = new ReentrantLock();
         registro = new ArrayList();
         pendientes = new LinkedBlockingQueue();
-        obtenerEnlaces(url);
-        ExecutorService executor = Executors.newScheduledThreadPool(registro.size());
-        
-        for(String enlace : pendientes){
-            executor.execute(new Hilo(enlace, rl, registro, pendientes));
+        System.out.println("Obteniendo enlaces");
+        obtenerEnlaces(url, profundidad);
+        System.out.println("Descargando... ");
+        ExecutorService executor = Executors.newScheduledThreadPool(32);
+        for(int i = 0; i < 32; i++){
+            executor.execute(new Hilo(rl, registro, pendientes, i));
         }
+        System.out.println("Esperando a que todo termine");
         executor.shutdown();
         while(!executor.isTerminated()){
             
         }
         System.out.println("Ya acabó");
+        System.out.println("Enlaces descargados: "+registro.size());
     }
     public static void main(String[] args) {
         try {
@@ -44,17 +49,38 @@ public class ExamenURL{
         }
     }
     
-    public void obtenerEnlaces(String url) throws InterruptedException{
+    public void obtenerEnlaces(String url, int profundidad){
         try{
             Document doc =  Jsoup.connect(url).ignoreHttpErrors(false).get();
             Elements enlaces = doc.select("a[href^='http']");
             for(Element enlace : enlaces){
                 pendientes.put(enlace.attr("href"));
+                if(profundidad != 0){
+                    obtenerEnlaces(enlace.attr("href"), profundidad - 1);    
+                }
             }
+            System.out.println("Enlaces obtenidos: "+pendientes.size());
+            for(Element enlace: enlaces){
+                enlace.attr("href", analizador(enlace.attr("href"), "(?<=//).+")+".html");
+            }
+            
+            BufferedWriter writer = new BufferedWriter(new FileWriter("index.html"));
+            writer.write(doc.outerHtml());
+            writer.close();
         }
         catch(IOException e){
+            System.out.println("No se pudo descargar: "+e.getMessage());
+        } catch (InterruptedException e) {
             System.out.println("Valió barriga: "+e.getMessage());
         }
-        
+    }
+    private String analizador(String com, String regex) {
+        Pattern expreg = Pattern.compile(regex);
+        Matcher buscador = expreg.matcher(com);
+        if(buscador.find()){
+            return buscador.group().replace('/', '-');
+        }else{
+            return "hue";
+        }
     }
 }
--- Redes2/ExamenURL/src/examenurl/Hilo.java
+++ Redes2/ExamenURL/src/examenurl/Hilo.java
@@ -1,8 +1,6 @@
 package examenurl;
 
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,37 +14,49 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 public class Hilo implements Runnable{
-    private final String url;
+    private String url;
     private final ReentrantLock rl;
     private final ArrayList<String> registro;
     private final LinkedBlockingQueue<String> pendientes;
+    private final int numHilo;
     
-    public Hilo(String url, ReentrantLock rl, ArrayList<String> registro, LinkedBlockingQueue<String> pendientes){
-        this.url = url;
+    public Hilo(ReentrantLock rl, ArrayList<String> registro, LinkedBlockingQueue<String> pendientes, int hilo) throws InterruptedException{
         this.rl = rl;
         this.registro = registro;
         this.pendientes = pendientes;
+        this.numHilo = hilo;
     }
     
     @Override
     public void run() {
-        try{
-            rl.lock();
-            if(registro.contains(this.url)){
-                return;
+        while(true){
+            this.url = pendientes.poll();
+            try{
+                rl.lock();
+                if(this.url == null){
+                    System.out.println("Fin de hilo: "+this.numHilo);
+                    rl.unlock();
+                    break;
+                }
+                rl.unlock();
+                
+                while(registro.contains(this.url)){
+                    this.url = pendientes.poll();
+                }
+                
+                Document doc =  Jsoup.connect(this.url).ignoreHttpErrors(false).get();
+                Elements enlaces = doc.select("a[href^='http']");
+                for(Element enlace : enlaces){
+                    enlace.attr("href", analizador(enlace.attr("href"), "(?<=//).+")+".html");
+                }
+                BufferedWriter writer = new BufferedWriter(new FileWriter(analizador(this.url,"(?<=//).+")+".html"));
+                writer.write(doc.outerHtml());
+                writer.close();
+                registro.add(this.url);
             }
-            rl.unlock();
-            Document doc =  Jsoup.connect(this.url).ignoreHttpErrors(false).get();
-            Elements enlaces = doc.select("a[href^='http']");
-            for(Element enlace : enlaces){
-                pendientes.put(enlace.attr("href"));
+            catch(IOException ex) {
+                System.out.println("Valió barriga: "+ex.getMessage());
             }
-            BufferedWriter writer = new BufferedWriter(new FileWriter(analizador(this.url,"(?<=//).+")+".html"));
-            writer.write(doc.outerHtml());
-            writer.close();
-        }
-        catch(IOException | InterruptedException ex) {
-            System.out.println("Valió barriga: "+ex.getMessage());
         }
     }
     private String analizador(String com, String regex) {
